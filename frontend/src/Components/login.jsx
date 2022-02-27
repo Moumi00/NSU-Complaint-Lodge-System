062a48843d9e73d0c6c4adc6d5437deb1c501871
OmarHaroon01 <omarharoon01@yahoo.com>
2022-02-27 23:33:36 +0600

    handled Login Errors

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,6 +1,27 @@
 import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [emailErrorClass, setEmailErrorClass] = useState("none");
+
+  const validateEmail = () => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  function handleLoginButtonClicked(e) {
+    e.preventDefault();
+
+    console.log(email)
+    if (validateEmail()){
+      setEmailErrorClass("block");
+      return;
+    }
+  }
+
   return (
     <div class="flex-grow-1 background-color d-flex align-items-center justify-content-center">
       <div class="row justify-content-center w-100">
@@ -8,14 +29,17 @@ function Login() {
           <div class="separator my-5 ">
             <h1 className="text-dark fw-light">Login</h1>
           </div>
-          <form>
+          <form onSubmit={handleLoginButtonClicked}>
             <div class="form-group mb-4">
               <input
                 type="text"
                 class="form-control"
                 id="emailInput"
                 placeholder="Email"
+                onInput={(e)=>setEmail(e.target.value)}
+                onChange={(e)=>setEmailErrorClass("none")}
               ></input>
+              <span class={"text-danger d-" + emailErrorClass}>Enter a valid Email</span>
             </div>
             <div class="form-group mb-4">
               <input
