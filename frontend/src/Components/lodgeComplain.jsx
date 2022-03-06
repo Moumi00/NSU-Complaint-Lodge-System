45625f07008fd826037809748238ebd248b3feca
Moumi <mohsinamatinmoumi@Mohsinas-Air.lan>
2022-03-07 00:14:26 +0600

    Creating searchbar in lodge complain

--- frontend/src/Components/lodgeComplain.jsx
+++ frontend/src/Components/lodgeComplain.jsx
@@ -1,7 +1,46 @@
+import React, { useState } from "react";
+import Multiselect from 'multiselect-react-dropdown';
+
 function LodgeComplain() {
+  const items = [
+    { name: "Option 1", id: 1 },
+    { name: "Option 2", id: 2 },
+  ];
   return (
-    <div className="flex-grow-1">
-      <h1>Lodge COMPLAIN page</h1>
+    <div class="flex-grow-1 background-color d-flex align-items-center justify-content-center">
+      <div class="row justify-content-center w-100">
+        <div class="col-xl-6 col-lg-7 col-md-9 col-11 my-4 primary-background-color px-lg-5 pb-5">
+          <div class="separator my-5 ">
+            <h1 className="text-dark fw-light">Lodge Complain</h1>
+          </div>
+          <form>
+            <div class="form-group mb-4">
+              <Multiselect
+                isObject={false}
+                onKeyPressFn={function noRefCheck() {}}
+                onRemove={function noRefCheck() {}}
+                onSearch={function noRefCheck() {}}
+                onSelect={function noRefCheck() {}}
+                options={items}
+                placeholder={"Complain Against?"}
+              />
+            </div>
+            <div class="form-group mb-4">
+              <input
+                type="password"
+                class="form-control"
+                id="passwordInput"
+                placeholder="Evidence"
+              ></input>
+            </div>
+            <div className="d-block">
+              <button type="submit" class="btn btn-primary w-100 fw-bold">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
