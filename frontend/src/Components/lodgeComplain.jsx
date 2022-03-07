d58e69d7b1a2598c8753a45b2bd68e2f9f396207
OmarHaroon01 <omarharoon01@yahoo.com>
2022-03-07 22:54:40 +0600

    lodge complain updated

--- frontend/src/Components/lodgeComplain.jsx
+++ frontend/src/Components/lodgeComplain.jsx
@@ -1,37 +1,79 @@
+import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
-import Multiselect from 'multiselect-react-dropdown';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Select from "react-select";
 
 function LodgeComplain() {
-  const items = [
-    { name: "Option 1", id: 1 },
-    { name: "Option 2", id: 2 },
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
   ];
+
+  const updateList = function () {
+    var input = document.getElementById("file");
+    var output = document.getElementById("fileList");
+    var children = "";
+    for (var i = 0; i < input.files.length; ++i) {
+      children += "<li>" + input.files.item(i).name + "</li>";
+    }
+    output.innerHTML = "<ul>" + children + "</ul>";
+  };
+
   return (
     <div class="flex-grow-1 background-color d-flex align-items-center justify-content-center">
       <div class="row justify-content-center w-100">
-        <div class="col-xl-6 col-lg-7 col-md-9 col-11 my-4 primary-background-color px-lg-5 pb-5">
+        <div class="col-xl-5 col-lg-7 col-md-9 col-11 my-4 primary-background-color px-lg-5 pb-5">
           <div class="separator my-5 ">
             <h1 className="text-dark fw-light">Lodge Complain</h1>
           </div>
           <form>
             <div class="form-group mb-4">
-              <Multiselect
-                isObject={false}
-                onKeyPressFn={function noRefCheck() {}}
-                onRemove={function noRefCheck() {}}
-                onSearch={function noRefCheck() {}}
-                onSelect={function noRefCheck() {}}
-                options={items}
-                placeholder={"Complain Against?"}
-              />
+              <input
+                type="text"
+                class="form-control"
+                id="emailInput"
+                placeholder="Complain Title"
+              ></input>
             </div>
             <div class="form-group mb-4">
-              <input
-                type="password"
+              <textarea
                 class="form-control"
-                id="passwordInput"
-                placeholder="Evidence"
+                id="form4Example3"
+                rows="6"
+                placeholder="Complain Description (upto 150 words)"
+                style={{ resize: "none" }}
+              ></textarea>
+            </div>
+            <div className="form-group d-flex flex-column mb-4">
+              <div className="col-12">
+                <Select
+                  options={options}
+                  placeholder={
+                    <div style={{ color: "grey" }}>Complain Against</div>
+                  }
+                  components={{
+                    DropdownIndicator: () => null, // Remove dropdown icon
+                    IndicatorSeparator: () => null, // Remove separator
+                  }}
+                  isMulti
+                />
+              </div>
+            </div>
+            <div class="form-group mb-4">
+              <label class="button-attach" for="file">
+                <i class="bi bi-paperclip me-1"></i>
+                Evidence
+              </label>
+              <input
+                accept="image/*, application/pdf"
+                id="file"
+                type="file"
+                name="file"
+                multiple
+                onChange={updateList}
               ></input>
+              <div id="fileList"></div>
             </div>
             <div className="d-block">
               <button type="submit" class="btn btn-primary w-100 fw-bold">
