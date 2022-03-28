3d94bc23d76cbfc6600ddcf37c58049d17ae58c6
Moumi <mohsinamatinmoumi@Mohsinas-Air.lan>
2022-03-28 23:21:12 +0600

    lodge complaint backend updated

--- Project/backend/Routes/home.js
+++ Project/backend/Routes/home.js
@@ -33,42 +33,21 @@ router.post("/lodge-complaint", async (req, res) => {
       ComplainAgainstUserUNID: req.body.complainAgainstUserUNID[i],
     });
   }
-  // res.send()
-  // for (let i = 0; i < req.body.evidence.length; i++){
-  //     await Evidence.create({
-  //         ComplainUNID: complainUNID,
-  //         evidence:
-  //     })
-  // }
+
   await ComplainReviewer.create({
     ComplainUNID: complainUNID,
     ComplainReviewerUserUNID: req.body.complainReviewerUserUNID,
   });
-  res.json("Successful!!YAAAAY");
-});
 
-router.post("/hudaai", async (req, res) => {
-  // console.log(req.files.file[0]);
-  //   let sampleFile = req.files.file;
-  //   res.send(sampleFile.length);
-  if (!req.files) {
-    return res.send("No files were uploaded.");
-  }
-//   let uploadPath;
-//   const file = req.files.file;
-//   uploadPath = path.join(__dirname, "..");
-//   uploadPath +=
-//     "/uploads/Evidence/" + file.filename + "." + file.name.split(".").pop();
-//   file.mv(uploadPath, function (err) {
-//     if (err) {
-//       return res.json({
-//         data: "",
-//         error: "File Upload Error",
-//       });
-//     }
-//   });
-  res.send(req.files);
-  console.log(req.files);
+  await ComplainDescription.create({
+    ComplainUNID: complainUNID,
+    complainDescription: req.body.complainDescription,
+  });
+  
+  res.json({
+    data: "Succesfully lodged a complaint",
+    error: ""
+  });
 });
 
 module.exports = router;
