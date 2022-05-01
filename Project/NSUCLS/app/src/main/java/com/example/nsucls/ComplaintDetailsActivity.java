dca1cca13bf1b09cdf1ea00a88e0ed6ff2800075
Moumi <mohsinamatinmoumi@Mohsinas-MacBook-Air.local>
2022-05-01 18:15:27 +0600

    Creating Complaint Details mobile ap

---
+++ Project/NSUCLS/app/src/main/java/com/example/nsucls/ComplaintDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.nsucls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_details);
+    }
+}
